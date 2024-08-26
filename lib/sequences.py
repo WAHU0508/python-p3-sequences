b085a30ec132aa7d71f4861483b37111e3d3fbe8
WAHU0508 <sharongikenye@gmail.com>
2024-08-26 12:37:06 +0300

    Fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_fibonacci = []
+    if (length == 0):
+        print(my_fibonacci)
+    elif (length == 1):
+        my_fibonacci.append(0)
+        print(my_fibonacci)
+    elif (length == 2):
+        my_fibonacci.append(0)
+        my_fibonacci.append(1)
+        print(my_fibonacci)
+    else:
+        my_fibonacci.append(0)
+        my_fibonacci.append(1)
+        i = 2
+        while i < length:
+            next_fibonacci = my_fibonacci[i - 1] + my_fibonacci[i - 2]
+            my_fibonacci.append(next_fibonacci)
+            i += 1
+        print(my_fibonacci)
